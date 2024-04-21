64af58e34cb1bd0b281d7e576b315e2ce4c9e1dd
newbee1905 <beenewminh@outlook.com>
2024-04-21 12:32:49 +1000

    feat: add in average accuracy per class metric

--- core/models/attention_net.py
+++ core/models/attention_net.py
@@ -13,6 +13,7 @@ import torch
 import numpy as np
 from config import CAT_NUM, PROPOSAL_NUM
 
+from collections import defaultdict
 from tempfile import TemporaryDirectory
 import time
 from tqdm import tqdm
@@ -192,10 +193,11 @@ class Model(BasicModel):
 			- dl: torch.utils.data.Dataloader
 
 		:return
-			- epoch_loss (float): mean loss of that epoch
-			- epoch_acc (float): mean accuarcy of that epoch
+			- epoch_loss: mean loss of that epoch
+			- epoch_acc: mean accuarcy of that epoch
+			- epoch_apc: mean accuarcy per class of that epoch
 
-		:rtype: Tuple[float, float, bool]
+		:rtype: Tuple[float, float, float]
 		"""
 		if phase == 'train':
 			self.model.train()
@@ -204,6 +206,8 @@ class Model(BasicModel):
 
 		running_loss = 0.0
 		running_corrects = 0
+		class_corrects = defaultdict(int)
+		class_counts = defaultdict(int)
 
 		for i, data in enumerate(tqdm(dl)):
 			X, y = data[0].to(self.device), data[1].to(self.device)
@@ -243,13 +247,22 @@ class Model(BasicModel):
 			running_loss += concat_loss.item() * X.size(0)
 			running_corrects += torch.sum(y_hat == y.data)
 
+			for i in range(200):
+				class_mask = (y == i)
+				class_corrects[i] += torch.sum(y_hat[class_mask] == y[class_mask]).item()
+				class_counts[i] += torch.sum(class_mask).item()
+
 
 		ds_size = len(dl.dataset)
 		epoch_loss = running_loss / ds_size
 		epoch_acc = running_corrects.double() / ds_size
-		
-		print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
-		return epoch_loss, epoch_acc
+
+		class_acc = {i: class_corrects[i] / class_counts[i] if class_counts[i] != 0 else 0 for i in range(200)}
+
+		epoch_apc = sum(class_acc.values()) / len(class_acc)
+
+		print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}, Apc: {epoch_apc:.4f}")
+		return epoch_loss, epoch_acc, epoch_apc
 
 	def _fit(self, train_dl, val_dl, num_epochs):
 		"""
@@ -278,7 +291,7 @@ class Model(BasicModel):
 				print(f"Epoch {epoch + 1}/{num_epochs}")
 				
 				self._train("train", train_dl)
-				val_loss, val_acc = self._train("val", val_dl)
+				val_loss, val_acc, _ = self._train("val", val_dl)
 
 				if val_acc > best_acc:
 					best_acc = val_acc 
--- core/models/transfer_cnn.py
+++ core/models/transfer_cnn.py
@@ -9,6 +9,7 @@ from torch.optim import lr_scheduler
 import torch
 from config import BATCH_SIZE
 
+from collections import defaultdict
 from tempfile import TemporaryDirectory
 import time
 from tqdm import tqdm
@@ -62,8 +63,9 @@ class Model(BasicModel):
 			- dl: torch.utils.data.Dataloader
 
 		:return
-			- epoch_loss (float): mean loss of that epoch
-			- epoch_acc (float): mean accuarcy of that epoch
+			- epoch_loss: mean loss of that epoch
+			- epoch_acc: mean accuarcy of that epoch
+			- epoch_apc: mean accuarcy per class of that epoch
 			- is_early_stop (float): check if stop after epoch for early stop
 
 		:rtype: Tuple[float, float, bool]
@@ -76,6 +78,8 @@ class Model(BasicModel):
 		is_early_stop = False
 		running_loss = 0.0
 		running_corrects = 0
+		class_corrects = defaultdict(int)
+		class_counts = defaultdict(int)
 
 		for i, data in enumerate(tqdm(dl)):
 			X, y = data[0].to(self.device), data[1].to(self.device)
@@ -93,16 +97,25 @@ class Model(BasicModel):
 			running_loss += loss.item() * X.size(0)
 			running_corrects += torch.sum(y_hat == y.data)
 
+			for i in range(200):
+				class_mask = (y == i)
+				class_corrects[i] += torch.sum(y_hat[class_mask] == y[class_mask]).item()
+				class_counts[i] += torch.sum(class_mask).item()
+
 
 		ds_size = len(dl.dataset)
 		epoch_loss = running_loss / ds_size
 		epoch_acc = running_corrects.double() / ds_size
 
+		class_acc = {i: class_corrects[i] / class_counts[i] if class_counts[i] != 0 else 0 for i in range(200)}
+
+		epoch_apc = sum(class_acc.values()) / len(class_acc)
+
 		if phase == 'val' and self.early_stopper.early_stop(epoch_loss):
 			is_early_stop = True
 		
-		print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
-		return epoch_loss, epoch_acc, is_early_stop
+		print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}, Apc: {epoch_apc:.4f}")
+		return epoch_loss, epoch_acc, epoch_apc, is_early_stop
 
 	def _fit(self, train_dl, val_dl, num_epochs):
 		"""
@@ -131,7 +144,7 @@ class Model(BasicModel):
 				print(f"Epoch {epoch + 1}/{num_epochs}")
 				
 				self._train("train", train_dl)
-				val_loss, val_acc, is_early_stop = self._train("val", val_dl)
+				val_loss, val_acc, _, is_early_stop = self._train("val", val_dl)
 
 				if val_acc > best_acc:
 					best_acc = val_acc 
